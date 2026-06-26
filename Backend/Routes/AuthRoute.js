72a41ff9bbf092925bcb2c08220a5d72f40f5ddd
Pari Rastogi <214537837+PariRastogi18@users.noreply.github.com>
2026-06-26 15:46:51 +0530

    commit

--- Backend/Routes/AuthRoute.js
+++ Backend/Routes/AuthRoute.js
@@ -3,7 +3,11 @@ const { userVerification } = require("../Middlewares/AuthMiddleware");
 const router = require("express").Router();
 
 router.post("/signup", Signup);
-router.post("/login", Login);
+router.post("/login", (req, res) => {
+  console.log("LOGIN HIT");
+  console.log(req.body);
+  res.json({ success: true });
+});
 router.post("/logout", Logout);
 router.post("/verify", userVerification);
 module.exports = router;
