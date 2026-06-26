9ecd03c1a76090f62bb662de2a2d2a5312557217
Pari Rastogi <214537837+PariRastogi18@users.noreply.github.com>
2026-06-26 10:13:37 +0530

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -28,7 +28,10 @@ app.use(
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: [
+      "https://stock-sphere-project-43auyjcpz-pari-rastogis-projects.vercel.app/",
+      "https://stock-sphere-project-mtil.vercel.app/",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
