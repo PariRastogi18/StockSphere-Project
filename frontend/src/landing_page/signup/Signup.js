c2219468f9ad5158e223a3359c07d916fba817d7
Pari Rastogi <214537837+PariRastogi18@users.noreply.github.com>
2026-06-26 19:38:07 +0530

    commit

--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -31,7 +31,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        `${process.env.REACT_APP_API_URL}/login`,
+        `${process.env.REACT_APP_API_URL}/signup`,
         {
           ...inputValue,
         },
